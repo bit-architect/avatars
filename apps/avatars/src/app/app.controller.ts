b0fcf190fb6bafc6f59a9ee590b3d94f1f7f6447
johannesschobel <johannes.schobel@googlemail.com>
2023-08-14 14:44:05 +0200

    feat: make service

--- apps/avatars/src/app/app.controller.ts
+++ apps/avatars/src/app/app.controller.ts
@@ -1,13 +1,45 @@
-import { Controller, Get } from '@nestjs/common';
-
+import { Controller, Get, Header, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+import { GeneratorOptions } from '@bit-architect/avatars-generator';
+
+const defaultAvatarOptions: GeneratorOptions = {
+  name: '',
+  colors: ['#264653', '#2a9d8f', '#e9c46a', '#f4a261', '#e76f51'],
+  size: 80,
+  isSquare: false,
+};
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getData() {
-    return this.appService.getData();
+  @Get('/:type/:name')
+  @Header('Content-Type', 'image/svg+xml')
+  getData(
+    @Param('type') type: string,
+    @Param('name') name: string,
+    @Query() query: any
+  ) {
+    const queryOptions: Partial<GeneratorOptions> = {};
+
+    queryOptions.name = name;
+    queryOptions.size = +query.size;
+
+    if ('square' in query) {
+      queryOptions.isSquare = true;
+    }
+
+    if ('colors' in query) {
+      queryOptions.colors = query.colors
+        .split(',')
+        .map((color: string) => (color.startsWith('#') ? color : `#${color}`));
+    }
+
+    const options: GeneratorOptions = {
+      ...defaultAvatarOptions,
+      ...queryOptions,
+    };
+
+    return this.appService.createAvatar(type, options);
   }
 }
--- apps/avatars/src/app/app.service.ts
+++ apps/avatars/src/app/app.service.ts
@@ -1,17 +1,43 @@
-import { AvatarBauhaus } from '@bit-architect/avatars-generator';
-import { Injectable } from '@nestjs/common';
+import {
+  Avatar,
+  AvatarBauhaus,
+  GeneratorOptions,
+} from '@bit-architect/avatars-generator';
+import { BadRequestException, Injectable } from '@nestjs/common';
 
+const availableTypes = ['bauhaus', 'beam', 'marble', 'pixel', 'ring', 'sunset'];
 @Injectable()
 export class AppService {
-  getData(): { avatar: string } {
-    const options = {
-      name: 'asfda8s7xfzasefa',
-      colors: ['#264653', '#2a9d8f', '#e9c46a', '#f4a261', '#e76f51'],
-      size: 80,
-      isSquare: false,
-    };
+  createAvatar(type: string, options: GeneratorOptions): string {
+    let avatarBuilder: Avatar = null;
+    switch (type) {
+      case 'bauhaus':
+        avatarBuilder = new AvatarBauhaus();
+        break;
+      case 'beam':
+        avatarBuilder = new AvatarBauhaus();
+        break;
+      case 'marble':
+        avatarBuilder = new AvatarBauhaus();
+        break;
+      case 'pixel':
+        avatarBuilder = new AvatarBauhaus();
+        break;
+      case 'ring':
+        avatarBuilder = new AvatarBauhaus();
+        break;
+      case 'sunset':
+        avatarBuilder = new AvatarBauhaus();
+        break;
+      default:
+        throw new BadRequestException(
+          `Avatar type "${type}" is not supported. Supported types are: ${availableTypes.join(
+            ', '
+          )}.`
+        );
+    }
 
-    const avatar = new AvatarBauhaus().generate(options);
-    return { avatar: avatar };
+    const avatar = avatarBuilder.generate(options);
+    return avatar;
   }
 }
