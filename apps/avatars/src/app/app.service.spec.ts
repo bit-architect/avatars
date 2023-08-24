86b383dcef6e8f0a7b75ab01fda110b6d60d23d0
johannesschobel <johannes.schobel@googlemail.com>
2023-08-24 10:40:48 +0200

    refactor: setup ci and stuff

--- apps/avatars/src/app/app.service.spec.ts
+++ apps/avatars/src/app/app.service.spec.ts
@@ -14,8 +14,8 @@ describe('AppService', () => {
   });
 
   describe('getData', () => {
-    it('should return "Hello API"', () => {
-      expect(service.getData()).toEqual({message: 'Hello API'});
+    it('should be defined', () => {
+      expect(service).toBeDefined();
     });
   });
 });
